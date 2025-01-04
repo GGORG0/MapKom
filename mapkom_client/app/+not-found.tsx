3661d76e3fcff3403f732a2e017dd5e6d7a41840
GGORG <GGORG0@protonmail.com>
2025-01-05 00:08:17 +0100

    client: tweaked not found page

--- mapkom_client/app/+not-found.tsx
+++ mapkom_client/app/+not-found.tsx
@@ -1,17 +1,21 @@
-import { Link, Stack } from 'expo-router';
+import { Link, Stack, usePathname } from 'expo-router';
 import React from 'react';
 import { Button, Surface, Text } from 'react-native-paper';
 import { styles } from '@/lib/styles';
 import { useTranslation } from 'react-i18next';
+import { StyleSheet } from 'react-native';
 
 export default function NotFoundScreen() {
   const { t } = useTranslation();
+  const path = usePathname();
 
   return (
     <Surface style={styles.screen}>
       <Stack.Screen options={{ title: t('notFoundPage.title') }} />
 
       <Text variant="displayLarge">{t('notFoundPage.title')}</Text>
+
+      <Text style={localStyles.pathDisplay}>{path}</Text>
 
       <Link href="/" asChild>
         <Button icon="home" mode="contained">
@@ -21,3 +25,9 @@ export default function NotFoundScreen() {
     </Surface>
   );
 }
+
+const localStyles = StyleSheet.create({
+  pathDisplay: {
+    fontFamily: 'monospace',
+  },
+});
